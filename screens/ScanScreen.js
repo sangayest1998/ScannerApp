6271fd97c8b5c848a41204cb7fe2505846ff9c62
sangayest1998 <127473845+sangayest1998@users.noreply.github.com>
2023-10-20 21:23:36 +0600

    y

--- screens/ScanScreen.js
+++ screens/ScanScreen.js
@@ -1,146 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import { View, Text, StyleSheet, ActivityIndicator, Alert } from 'react-native';
-// import { BarCodeScanner } from 'expo-barcode-scanner';
-// import axios from 'axios';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
-
-// const ScanScreen = () => {
-//   const [hasPermission, setHasPermission] = useState(null);
-//   const [scanned, setScanned] = useState(false);
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [userId, setUserId] = useState('');
-
-//   useEffect(() => {
-//     (async () => {
-//       const { status } = await BarCodeScanner.requestPermissionsAsync();
-//       setHasPermission(status === 'granted');
-//     })();
-//   }, []);
-
-//   useEffect(() => {
-//     AsyncStorage.getItem('userId')
-//       .then((value) => {
-//         if (value) {
-//           setUserId(value);
-//         }
-//       })
-//       .catch((error) => {
-//         console.error('Error retrieving userId:', error);
-//       });
-//   }, []);
-
-//   const handleBarCodeScanned = async ({ type, data }) => {
-//     if (isLoading) return;
-
-//     setIsLoading(true);
-//     setScanned(true);
-
-//     try {
-//       const response = await axios.post('http://192.168.0.119:8000/api/qr-scan', {
-//         cid: userId,
-//         token: data,
-//       });
-
-//       console.log(response.data);
-
-//       if (response.data.message) {
-//         Alert.alert('Success', response.data.message, [
-//           { text: 'OK', onPress: resetScanner },
-//         ]);
-//       } else {
-//         Alert.alert('Unknown Status', 'An unknown status was returned from the server.', [
-//           { text: 'OK', onPress: resetScanner },
-//         ]);
-//       }
-//     } catch (error) {
-//       console.error(error);
-
-//       if (error.response && error.response.data && error.response.data.detail) {
-//         Alert.alert('Error', error.response.data.detail, [
-//           { text: 'OK', onPress: resetScanner },
-//         ]);
-//       } else {
-//         Alert.alert('Error', 'An error occurred while processing your request.', [
-//           { text: 'OK', onPress: resetScanner },
-//         ]);
-//       }
-//     } finally {
-//       setIsLoading(false);
-//     }
-//   };
-
-//   const resetScanner = () => {
-//     setScanned(false);
-//   };
-
-//   if (hasPermission === null) {
-//     return (
-//       <View style={styles.container}>
-//         <ActivityIndicator size="large" color="blue" />
-//         <Text>Requesting camera permission...</Text>
-//       </View>
-//     );
-//   }
-//   if (hasPermission === false) {
-//     return <Text style={styles.errorText}>No access to camera.</Text>;
-//   }
-
-//   return (
-//     <View style={styles.container}>
-//       <Text style={styles.txt}>Place the QR code inside the frame</Text>
-//       <View style={styles.cameraWindow}>
-//         <BarCodeScanner
-//           onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-//           style={styles.scanner} // Use a custom style for the scanner
-//         />
-//       </View>
-//       {isLoading && (
-//         <ActivityIndicator size="large" color="blue" style={styles.loadingIndicator} />
-//       )}
-//     </View>
-    
-//   );
-// };
-// const styles = StyleSheet.create({
-//   container: {
-//     alignItems: 'center',
-//     flex: 1,
-//     justifyContent: 'center',
-//     backgroundColor:'grey'
-//   },
-//   errorText: {
-//     fontSize: 18,
-//     fontWeight: 'bold',
-//     color: 'red',
-//   },
-//   loadingIndicator: {
-//     position: 'absolute',
-//     top: '50%',
-//     left: 0,
-//     right: 0,
-//   },
-//   cameraWindow: {
-//     width: '75%',
-//     aspectRatio: 1, // This will make the camera feed fit perfectly into the square
-//     borderRadius: 20,
-//     overflow: 'hidden',
-//     marginBottom: 30,
-//     borderWidth: 5,
-//     borderColor: 'orange',
-//   },
-//   scanner: {
-//     height: 379,
-//   },
-//   txt:{
-//     color:'white',
-//     fontSize:17,
-//     marginBottom:30
-//   }
-// });
-
-// export default ScanScreen;
-
-
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, ActivityIndicator, Alert ,Dimensions} from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
@@ -181,9 +38,9 @@ const ScanScreen = () => {
     try {
       // const response = await axios.post('http://192.168.128.8:8000/api/qr-scan', 
 
-      // const response = await axios.post('http://202.144.153.106:8000/api/qr-scan', 
+      const response = await axios.post('http://202.144.153.106:8000/api/qr-scan', 
 
-      const response = await axios.post('http://192.168.128.8:8000/api/qr-scan', 
+      // const response = await axios.post('http://192.168.128.8:8000/api/qr-scan', 
       
       {
         cid: userId,
@@ -349,4 +206,149 @@ const styles = StyleSheet.create({
 });
 
 export default ScanScreen;
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import { View, Text, StyleSheet, ActivityIndicator, Alert } from 'react-native';
+// import { BarCodeScanner } from 'expo-barcode-scanner';
+// import axios from 'axios';
+// import AsyncStorage from '@react-native-async-storage/async-storage';
+
+// const ScanScreen = () => {
+//   const [hasPermission, setHasPermission] = useState(null);
+//   const [scanned, setScanned] = useState(false);
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [userId, setUserId] = useState('');
+
+//   useEffect(() => {
+//     (async () => {
+//       const { status } = await BarCodeScanner.requestPermissionsAsync();
+//       setHasPermission(status === 'granted');
+//     })();
+//   }, []);
+
+//   useEffect(() => {
+//     AsyncStorage.getItem('userId')
+//       .then((value) => {
+//         if (value) {
+//           setUserId(value);
+//         }
+//       })
+//       .catch((error) => {
+//         console.error('Error retrieving userId:', error);
+//       });
+//   }, []);
+
+//   const handleBarCodeScanned = async ({ type, data }) => {
+//     if (isLoading) return;
+
+//     setIsLoading(true);
+//     setScanned(true);
+
+//     try {
+//       const response = await axios.post('http://192.168.0.119:8000/api/qr-scan', {
+//         cid: userId,
+//         token: data,
+//       });
+
+//       console.log(response.data);
+
+//       if (response.data.message) {
+//         Alert.alert('Success', response.data.message, [
+//           { text: 'OK', onPress: resetScanner },
+//         ]);
+//       } else {
+//         Alert.alert('Unknown Status', 'An unknown status was returned from the server.', [
+//           { text: 'OK', onPress: resetScanner },
+//         ]);
+//       }
+//     } catch (error) {
+//       console.error(error);
+
+//       if (error.response && error.response.data && error.response.data.detail) {
+//         Alert.alert('Error', error.response.data.detail, [
+//           { text: 'OK', onPress: resetScanner },
+//         ]);
+//       } else {
+//         Alert.alert('Error', 'An error occurred while processing your request.', [
+//           { text: 'OK', onPress: resetScanner },
+//         ]);
+//       }
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   };
+
+//   const resetScanner = () => {
+//     setScanned(false);
+//   };
+
+//   if (hasPermission === null) {
+//     return (
+//       <View style={styles.container}>
+//         <ActivityIndicator size="large" color="blue" />
+//         <Text>Requesting camera permission...</Text>
+//       </View>
+//     );
+//   }
+//   if (hasPermission === false) {
+//     return <Text style={styles.errorText}>No access to camera.</Text>;
+//   }
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.txt}>Place the QR code inside the frame</Text>
+//       <View style={styles.cameraWindow}>
+//         <BarCodeScanner
+//           onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+//           style={styles.scanner} // Use a custom style for the scanner
+//         />
+//       </View>
+//       {isLoading && (
+//         <ActivityIndicator size="large" color="blue" style={styles.loadingIndicator} />
+//       )}
+//     </View>
+    
+//   );
+// };
+// const styles = StyleSheet.create({
+//   container: {
+//     alignItems: 'center',
+//     flex: 1,
+//     justifyContent: 'center',
+//     backgroundColor:'grey'
+//   },
+//   errorText: {
+//     fontSize: 18,
+//     fontWeight: 'bold',
+//     color: 'red',
+//   },
+//   loadingIndicator: {
+//     position: 'absolute',
+//     top: '50%',
+//     left: 0,
+//     right: 0,
+//   },
+//   cameraWindow: {
+//     width: '75%',
+//     aspectRatio: 1, // This will make the camera feed fit perfectly into the square
+//     borderRadius: 20,
+//     overflow: 'hidden',
+//     marginBottom: 30,
+//     borderWidth: 5,
+//     borderColor: 'orange',
+//   },
+//   scanner: {
+//     height: 379,
+//   },
+//   txt:{
+//     color:'white',
+//     fontSize:17,
+//     marginBottom:30
+//   }
+// });
+
+// export default ScanScreen;
+
 
